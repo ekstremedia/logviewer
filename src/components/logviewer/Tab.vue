d652e4763af88256cf547927cad1bf443c606f4e
Terje Nesthus <terje@fdvhuset.no>
2023-08-14 21:24:50 +0200

    Lint

--- src/components/logviewer/Tab.vue
+++ src/components/logviewer/Tab.vue
@@ -1,36 +1,33 @@
 <template>
     <button
-      class="ml-1 px-4 py-0.5 text-lg font-medium rounded-t-lg rounded-b-none border-b-none transition duration-300 ease-in-out
-        hover:bg-gray-500 hover:text-gray-100 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-      :class="{
-        'bg-gray-700  border-x-gray-300  border-t-gray-300': active,
-        'text-gray-400': !active,
-      }"
-      @click="select"
+        class="ml-1 px-4 py-0.5 text-lg font-medium rounded-t-lg rounded-b-none border-b-none transition duration-300 ease-in-out hover:bg-gray-500 hover:text-gray-100 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+        :class="{
+            'bg-gray-700  border-x-gray-300  border-t-gray-300': active,
+            'text-gray-400': !active,
+        }"
+        @click="select"
     >
-      <slot></slot>
+        <slot></slot>
     </button>
-  </template>
-  
-  <script setup>
-  
-  // Define properties
-  const props = defineProps({
+</template>
+
+<script setup>
+// Define properties
+const props = defineProps({
     active: Boolean,
-  });
-  
-  // Define emits
-  const emit = defineEmits(['select']);
-  
-  // Function to emit select event
-  const select = () => {
-    emit('select');
-  };
-  </script>
-  
-  <style scoped>
-  button {
+});
+
+// Define emits
+const emit = defineEmits(["select"]);
+
+// Function to emit select event
+const select = () => {
+    emit("select");
+};
+</script>
+
+<style scoped>
+button {
     /* Custom styles if needed */
-  }
-  </style>
-  
+}
+</style>
--- src/components/settings/Settings.vue
+++ src/components/settings/Settings.vue
@@ -1,48 +1,55 @@
 <template>
     <div class="p-6 bg-gray-800 text-gray-50 rounded-md shadow-md">
-      <h1 class="text-2xl font-semibold mb-4">Settings</h1>
-      
-      <!-- Add Log Component -->
-      <AddLog @saved="loadLogs" />
-      
-      <!-- Saved Logs Section -->
+        <h1 class="text-2xl font-semibold mb-4">Settings</h1>
+
+        <!-- Add Log Component -->
+        <AddLog @saved="loadLogs" />
+
+        <!-- Saved Logs Section -->
         <div class="mt-4">
-        <h2 class="text-xl font-semibold mb-2">Saved Logs</h2>
-        <div v-for="(log, index) in logs" :key="index" class="mb-2 flex items-center">
-          <span class="font-medium text-gray-100">{{ log.name }}:</span>
-          <span class="text-gray-300 ml-2">{{ log.directory }}</span>
-          
-          <!-- Remove Log Button -->
-          <button @click="removeLog(index)" class="ml-2 p-1 rounded-md bg-red-500 text-white hover:bg-red-600">
-            Remove
-          </button>
+            <h2 class="text-xl font-semibold mb-2">Saved Logs</h2>
+            <div
+                v-for="(log, index) in logs"
+                :key="index"
+                class="mb-2 flex items-center"
+            >
+                <span class="font-medium text-gray-100">{{ log.name }}:</span>
+                <span class="text-gray-300 ml-2">{{ log.directory }}</span>
+
+                <!-- Remove Log Button -->
+                <button
+                    @click="removeLog(index)"
+                    class="ml-2 p-1 rounded-md bg-red-500 text-white hover:bg-red-600"
+                >
+                    Remove
+                </button>
+            </div>
         </div>
-      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, watchEffect } from 'vue';
-  import AddLog from './AddLog.vue';
-  
-  const logs = ref([]);
-  
-  // Function to load logs from localStorage
-  const loadLogs = () => {
-    logs.value = JSON.parse(localStorage.getItem('logs') || '[]');
-  };
-  
-  // Function to remove a log
-  const removeLog = (index) => {
+</template>
+
+<script setup>
+import { ref, watchEffect } from "vue";
+import AddLog from "./AddLog.vue";
+
+const logs = ref([]);
+
+// Function to load logs from localStorage
+const loadLogs = () => {
+    logs.value = JSON.parse(localStorage.getItem("logs") || "[]");
+};
+
+// Function to remove a log
+const removeLog = (index) => {
     logs.value.splice(index, 1);
-    localStorage.setItem('logs', JSON.stringify(logs.value));
-  };
-  
-  // Watch the logs and reload when there's a change
-  watchEffect(() => {
+    localStorage.setItem("logs", JSON.stringify(logs.value));
+};
+
+// Watch the logs and reload when there's a change
+watchEffect(() => {
     loadLogs();
-  });
-  
-  // Initial load of logs
-  loadLogs();
-  </script>
+});
+
+// Initial load of logs
+loadLogs();
+</script>
