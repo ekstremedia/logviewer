0c6c5208318b27a9643542de895000117cf44eb8
Terje Nesthus <terje@fdvhuset.no>
2023-08-14 23:58:56 +0200

    Fixed some bugs

--- electron/main.ts
+++ electron/main.ts
@@ -1,75 +1,89 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
-import { getWindowState, setWindowState } from './windowState';
-import path from 'node:path';
-import fs from 'fs';
+
+import { app, BrowserWindow, ipcMain } from "electron";
+import { getWindowState, setWindowState } from "./windowState";
+import path from "node:path";
+import fs from "fs";
 
 // Set the environment variables for DIST and PUBLIC
-process.env.DIST = path.join(__dirname, '../dist');
-process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public');
+process.env.DIST = path.join(__dirname, "../dist");
+process.env.PUBLIC = app.isPackaged
+    ? process.env.DIST
+    : path.join(process.env.DIST, "../public");
 
 // IPC handler to check if a directory exists
-ipcMain.handle('check-directory', (event, path) => fs.existsSync(path));
+ipcMain.handle("check-directory", (event, path) => fs.existsSync(path));
 
-ipcMain.handle('tail-log', (event, logPath) => {
-    let content = fs.readFileSync(logPath, 'utf-8');
-    let currentSize = content.length;
-  
-    // Watch the file for changes
-    fs.watchFile(logPath, (curr) => {
-      if (curr.size > currentSize) {
-        const newContent = fs.readFileSync(logPath, { encoding: 'utf-8', start: currentSize, end: curr.size });
-        content += newContent;
-  
-        // Update the current size
-        currentSize = curr.size;
-  
-        // Send the new content to the renderer process
-        event.sender.send('log-updated', newContent);
-      }
-    });
-  
-    // Return the initial content
+ipcMain.handle("tail-log", (event, logPath) => {
+    const content = fs.readFileSync(logPath, "utf-8");
     return content;
+});
+
+let watcher: fs.FSWatcher | null = null;
+
+ipcMain.handle("start-tail-log", (event, logPath) => {
+    // If there's an existing watcher, close it first
+    if (watcher) {
+        watcher.close();
+    }
+
+    // Watch the file for changes
+    watcher = fs.watch(logPath, () => {
+        // Notify the renderer process that the log has been updated
+        console.log("updated");
+        event.sender.send("log-updated", logPath);
+    });
+
+    return true; // You can return any value that indicates the success of the operation
+});
+app.on("window-all-closed", () => {
+    // If there's an existing watcher, close it when the app is closed
+    if (watcher) {
+      watcher.close();
+    }
+    win = null;
   });
-  
 let win: BrowserWindow | null;
-const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']; // Dev server URL if running in development mode
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"]; // Dev server URL if running in development mode
 
 function createWindow() {
-  // Retrieve stored window size and position or use default values
-  const { width, height, x, y } = getWindowState();
+    // Retrieve stored window size and position or use default values
+    const { width, height, x, y } = getWindowState();
 
-  // Create the browser window with the specified dimensions and preload script
-  win = new BrowserWindow({
-    width,
-    height,
-    x,
-    y,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+    // Create the browser window with the specified dimensions and preload script
+    win = new BrowserWindow({
+        width,
+        height,
+        x,
+        y,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
 
-  // Save window size and position when resized or moved
-  win.on('resize', () => setWindowState(win.getBounds()));
-  win.on('move', () => setWindowState(win.getBounds()));
+    // Save window size and position when resized or moved
+    win.on("resize", () => setWindowState(win.getBounds()));
+    win.on("move", () => setWindowState(win.getBounds()));
 
-  // Send a message to the renderer process when the window is ready
-  win.webContents.on('did-finish-load', () => win?.webContents.send('main-process-message', (new Date).toLocaleString()));
+    // Send a message to the renderer process when the window is ready
+    win.webContents.on("did-finish-load", () =>
+        win?.webContents.send(
+            "main-process-message",
+            new Date().toLocaleString()
+        )
+    );
 
-  // Load the application from the appropriate source depending on the mode (development or production)
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL); // Load from dev server in development mode
-  } else {
-    win.loadFile(path.join(process.env.DIST, 'index.html')); // Load from static file in production mode
-  }
+    // Load the application from the appropriate source depending on the mode (development or production)
+    if (VITE_DEV_SERVER_URL) {
+        win.loadURL(VITE_DEV_SERVER_URL); // Load from dev server in development mode
+    } else {
+        win.loadFile(path.join(process.env.DIST, "index.html")); // Load from static file in production mode
+    }
 }
 
 // Create the main window when the application is ready
 app.whenReady().then(createWindow);
 
 // Set the window to null when all windows are closed
-app.on('window-all-closed', () => {
-  win = null;
+app.on("window-all-closed", () => {
+    win = null;
 });
-      
--- src/components/logviewer/LogViewer.vue
+++ src/components/logviewer/LogViewer.vue
@@ -1,60 +1,74 @@
 <template>
-    <div class="overflow-y-scroll h-[90vh]">
+    <div ref="logContainer" class="overflow-y-scroll h-[90vh]">
         <pre>
-      {{ logContent }}
-    </pre>
+            {{ logContent }}
+        </pre>
     </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, onUnmounted, ref } from 'vue';
-  
-  // Prop for the log object
-  const props = defineProps({
-    log: Object,
-  });
-  
-  // Reactive variable to store the log content
-  const logContent = ref('');
-  
-  // Function to handle log updates
-  const handleLogUpdate = (event, newLines) => {
-    logContent.value += newLines;
-  };
-    
+</template>
 
-  const getLogDisplayPath = (log) => {
-  if (log.type === "daily") {
-    const date = new Date();
-    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
-      2,
-      "0"
-    )}-${String(date.getDate()).padStart(2, "0")}`;
-    return `${log.directory}/${log.filename}-${formattedDate}.log`;
-  } else {
-    return `${log.directory}/${log.filename}`;
+<script setup>
+import { onMounted, onUnmounted, ref, watch, nextTick } from "vue";
+
+// Prop for the log object
+const props = defineProps({
+    log: Object,
+});
+
+// Reactive variable to store the log content
+const logContent = ref("");
+
+// Reference to the log container
+const logContainer = ref(null);
+
+// Watch for changes in the log content
+watch(logContent, async () => {
+    // Wait for the next tick to ensure the DOM has been updated
+    await nextTick();
+
+    // Scroll to the bottom
+    if (logContainer.value) {
+        logContainer.value.scrollTop = logContainer.value.scrollHeight;
+    }
+});
+
+const getLogDisplayPath = (log) => {
+    if (log.type === "daily") {
+        const date = new Date();
+        const formattedDate = `${date.getFullYear()}-${String(
+            date.getMonth() + 1
+        ).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`;
+        return `${log.directory}/${log.filename}-${formattedDate}.log`;
+    } else {
+        return `${log.directory}/${log.filename}`;
+    }
+};
+
+// Function to load the log
+const loadLog = async () => {
+  const logPath = getLogDisplayPath(props.log);
+  
+  // Start tailing the log
+  await window.electron.invoke('start-tail-log', logPath);
+
+  // Load the content
+  const content = await window.electron.invoke('tail-log', logPath);
+  logContent.value = content;
+};
+
+// Function to handle log updates
+const handleLogUpdate = async (event, updatedLogPath) => {
+  if (updatedLogPath === getLogDisplayPath(props.log)) {
+    await loadLog();
   }
-  };
-  // Function to load the log
-  const loadLog = async () => {
-    const logPath = getLogDisplayPath(props.log); // Assuming you have this function in scope
-  
-    // Load the initial content
-    await window.electron
-      .invoke('tail-log', logPath)
-      .then((content) => {
-        logContent.value = content;
-      });
-  
-    // Listen for updates
-    window.electron.on('log-updated', handleLogUpdate);
-  };
-  
-  onUnmounted(() => {
-    // Clean up the listener
-    window.electron.removeListener('log-updated', handleLogUpdate);
-  });
-  
-  onMounted(loadLog);
-  </script>
-  
+};
+
+// Listen for updates
+window.electron.on('log-updated', handleLogUpdate);
+
+onUnmounted(() => {
+  // Clean up the listener
+  window.electron.removeListener('log-updated', handleLogUpdate);
+});
+
+onMounted(loadLog);
+</script>
